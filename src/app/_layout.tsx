0815295fac445719e763580d8d448cf3ae6a89c2
ivanleopoldo <ivanleopoldoc@gmail.com>
2026-03-21 23:11:44 +0800

    chore: some random things

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,35 @@
-import { Stack } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { defaultConfig } from "@tamagui/config/v5";
+import { router, Stack } from "expo-router";
+import { Pressable } from "react-native";
+import { createTamagui, styled, TamaguiProvider, View } from "tamagui";
+
+const config = createTamagui(defaultConfig);
+
+const Icon = styled(Ionicons);
 
 export default function RootLayout() {
-  return <Stack />;
+	return (
+		<TamaguiProvider defaultTheme={"light"} config={config}>
+			<Stack>
+				<Stack.Screen
+					name="index"
+					options={{
+						headerTitle: "Home",
+						headerRight: () => (
+							<Pressable onPress={() => router.push("/settings")}>
+								<View marginLeft={5.5}>
+									<Icon name="settings-sharp" size={24} color="black" />
+								</View>
+							</Pressable>
+						),
+					}}
+				/>
+				<Stack.Screen
+					name="settings"
+					options={{ title: "Settings", presentation: "modal" }}
+				/>
+			</Stack>
+		</TamaguiProvider>
+	);
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,37 @@
-import { Text, View } from "react-native";
+import { Pressable } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import type { PressableProps } from "react-native/Libraries/Components/Pressable/Pressable";
+import { styled, Text, View, YGroup } from "tamagui";
+
+const StyledSafeAreaView = styled(SafeAreaView);
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+	const titles = ["Home", "Work", "Groceries", "Chores", "Other"];
+
+	return (
+		<StyledSafeAreaView padding={10}>
+			<YGroup>
+				{titles.map((title) => (
+					<YGroup.Item key={title}>
+						<TodoItem title={title} />
+					</YGroup.Item>
+				))}
+			</YGroup>
+		</StyledSafeAreaView>
+	);
 }
+
+const TodoItem = ({ title, ...props }: { title?: string } & PressableProps) => {
+	return (
+		<Pressable {...props}>
+			<View
+				backgroundColor="$backgroundHover"
+				borderRadius="$4"
+				padding="$4"
+				marginBottom="$2"
+			>
+				<Text>{title}</Text>
+			</View>
+		</Pressable>
+	);
+};
