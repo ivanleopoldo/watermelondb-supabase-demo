bacdd4e3476f4e0941ce3ad141dac78e423495c1
ivanleopoldo <ivanleopoldoc@gmail.com>
2026-03-23 03:23:12 +0800

    feat: adds signout button in settings and clear database

--- src/app/settings.tsx
+++ src/app/settings.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "tamagui";
+import { useAuth } from "../hooks/useAuth";
+import { database } from "../lib/db";
 
 export default function Settings() {
-	return (
-		<View>
-			<Text>Settings</Text>
-		</View>
-	);
+  const { signOut } = useAuth();
+
+  return (
+    <View>
+      <Text>Settings</Text>
+      <Button
+        onPress={async () => {
+          signOut();
+          await database.write(() => database.unsafeResetDatabase());
+        }}
+      >
+        <Text>Sign Out</Text>
+      </Button>
+    </View>
+  );
 }
